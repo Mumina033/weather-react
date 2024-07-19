96cd43c0b1d1357491d298409ae0dd3894202c23
Mumina033 <mumina.mohamed033@gmail.com>
2024-07-19 09:19:39 -0400

    made New Orleans the default city

--- src/Search.js
+++ src/Search.js
@@ -1,39 +1,20 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./index.css";
 import axios from "axios";
 
 export default function Search() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("New Orleans");
   const [loaded, setLoaded] = useState(false);
   const [weather, setWeather] = useState(null);
-  const [forecast, setForecast] = useState([]);
 
-  function generateRandomForecast() {
-    const icons = [
-      "01d",
-      "02d",
-      "03d",
-      "04d",
-      "09d",
-      "10d",
-      "11d",
-      "13d",
-      "50d",
-      "01n",
-      "02n",
-      "03n",
-      "04n",
-      "09n",
-      "10n",
-      "11n",
-      "13n",
-      "50n",
-    ];
+  useEffect(() => {
+    fetchWeather("New Orleans");
+  }, []);
 
-    const randomIcons = icons.sort(() => 0.5 - Math.random()).slice(0, 5);
-    return randomIcons.map(
-      (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`
-    );
+  function fetchWeather(city) {
+    let apiKey = "d1a86552de255334f6117b348c4519bd";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather).catch(handleError);
   }
 
   function displayWeather(response) {
@@ -45,8 +26,6 @@ export default function Search() {
       icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
     });
-
-    setForecast(generateRandomForecast());
   }
 
   function handleError(error) {
@@ -62,11 +41,13 @@ export default function Search() {
       alert("Please enter a city name.");
       return;
     }
+    fetchWeather(city);
+  }
 
-    let apiKey = "d1a86552de255334f6117b348c4519bd";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    console.log("API URL:", apiUrl);
-    axios.get(apiUrl).then(displayWeather).catch(handleError);
+  function updateCity(event) {
+    let input = event.target.value;
+    let capitalizedInput = capitalizeCityName(input);
+    setCity(capitalizedInput);
   }
 
   function capitalizeCityName(name) {
@@ -74,12 +55,6 @@ export default function Search() {
       .split(" ")
       .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(" ");
-  }
-
-  function updateCity(event) {
-    let input = event.target.value;
-    let capitalizedInput = capitalizeCityName(input);
-    setCity(capitalizedInput);
   }
 
   let form = (
@@ -98,23 +73,69 @@ export default function Search() {
     </form>
   );
 
-  let dailyForecast = (
+  let dailyForecast = weather ? (
     <div className="container">
       <div className="row">
-        {["Mon", "Tues", "Wens", "Thurs", "Fri"].map((day, index) => (
-          <div className="col-md" key={index}>
-            <div className="forecast-day">{day}</div>
-            <div className="forecast-content">
-              <div className="forecast-img">
-                <img src={forecast[index]} alt={`Forecast for ${day}`} />
-              </div>
-              <div className="forecast-max-min">
-                <span className="forecast-max">34°</span>
-                <span className="forecast-min">23°</span>
-              </div>
+        <div className="col-md">
+          <div className="forecast-day">Mon</div>
+          <div className="forecast-content">
+            <div className="forecast-img">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="forecast-max-min">
+              <span className="forecast-max">34°</span>
+              <span className="forecast-min">23°</span>
             </div>
           </div>
-        ))}
+        </div>
+        <div className="col-md">
+          <div className="forecast-day">Tues</div>
+          <div className="forecast-content">
+            <div className="forecast-img">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="forecast-max-min">
+              <span className="forecast-max">34°</span>
+              <span className="forecast-min">23°</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="forecast-day">Wens</div>
+          <div className="forecast-content">
+            <div className="forecast-img">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="forecast-max-min">
+              <span className="forecast-max">34°</span>
+              <span className="forecast-min">23°</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="forecast-day">Thurs</div>
+          <div className="forecast-content">
+            <div className="forecast-img">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="forecast-max-min">
+              <span className="forecast-max">34°</span>
+              <span className="forecast-min">23°</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-md">
+          <div className="forecast-day">Fri</div>
+          <div className="forecast-content">
+            <div className="forecast-img">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+            <div className="forecast-max-min">
+              <span className="forecast-max">34°</span>
+              <span className="forecast-min">23°</span>
+            </div>
+          </div>
+        </div>
       </div>
       <footer className="footer-css">
         This project was coded by{" "}
@@ -143,31 +164,31 @@ export default function Search() {
         </a>
       </footer>
     </div>
-  );
+  ) : null;
 
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <div className="col-6">
-          <h2>{city}</h2>
-          Friday 3:57 AM, {weather.description}
-          <p className="weather-info">
-            Humidity: <span className="highlight">{weather.humidity}% </span>,
-            Wind:{" "}
-            <span className="highlight">{Math.round(weather.wind)} mph</span>
-          </p>
-        </div>
-        <div className="col-8">
+  return (
+    <div>
+      {form}
+      <div className="col-6">
+        <h2>{city}</h2>
+        {weather ? (
+          <>
+            Friday 3:57 AM, {weather.description}
+            <p className="weather-info">
+              Humidity: {weather.humidity}%, Wind: {weather.wind} mph
+            </p>
+          </>
+        ) : null}
+      </div>
+      <div className="col-8">
+        {weather ? (
           <h1 className="initialtemp">
             <img src={weather.icon} alt={weather.description} />{" "}
             {Math.round(weather.temperature)}°C
           </h1>
-        </div>
-        {dailyForecast}
+        ) : null}
       </div>
-    );
-  } else {
-    return <div>{form}</div>;
-  }
+      {dailyForecast}
+    </div>
+  );
 }
